2fb067b4c81e2a16578bc247e1d2b2362365f010
Raymond <raymondsetrau@gmail.com>
2023-01-18 19:21:26 -0800

    Initialize Home Page and added images

---
+++ tmt-app/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="Home_Container">
+            <div>
+                Home page
+            </div>
+        </div>
+    )
+}
+
+export default Home;
