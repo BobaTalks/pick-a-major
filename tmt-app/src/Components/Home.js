60e9203a29bd1144370588d9bc8f01164918ca72
Raymond <raymondsetrau@gmail.com>
2023-01-18 20:35:49 -0800

    Home page path subheading

--- tmt-app/src/Components/Home.js
+++ tmt-app/src/Components/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import Forward from '../Images/Forward.png';
+import '../Styling/Home.css'
 function Home(){
     return(
         <div className="Home_Container">
-            <div>
-                Home page
+            <div className="Home_Path_Container">
+                <div className="Home_Path_Wrapper">
+                    <span>Student Projects</span>
+                    <img src={Forward}></img>
+                    <span>Major Picker</span>
+                </div>
             </div>
         </div>
     )
