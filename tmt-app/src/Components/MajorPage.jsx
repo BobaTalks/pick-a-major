63f45e0f3881f07448bfc7dc8cdbaeaba3e262fa
Raymond <raymondsetrau@gmail.com>
2023-05-01 23:21:18 -0700

    added title for majorpage

--- tmt-app/src/Components/MajorPage.jsx
+++ tmt-app/src/Components/MajorPage.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { MAJORS_LIST } from "../constants/careerMajorList";
 
 function MajorPage() {
-  const majorChoice = useParams();
+  const params = useParams();
+  const [data, setData] = useState([]);
 
-  return <div>{majorChoice.major}</div>;
+  const searchData = (target) => {
+    for (let i = 0; i < MAJORS_LIST.length; i++) {
+      if (target === MAJORS_LIST[i].key) {
+        setData(MAJORS_LIST[i]);
+        console.log(MAJORS_LIST[i]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    searchData(params.major);
+  }, [params]);
+
+  return (
+    <Container>
+      <Typography
+        variant="h1"
+        sx={{
+          margin: "0 auto",
+          textAlign: "center",
+        }}
+      >
+        {data.name} (Major)
+      </Typography>
+    </Container>
+  );
 }
 
 export default MajorPage;
