4973b7b4289677a7d83951566aa0b627596f64b1
Raymond <raymondsetrau@gmail.com>
2023-05-11 00:44:21 -0700

    add description div for major

--- tmt-app/src/Components/MajorPage.jsx
+++ tmt-app/src/Components/MajorPage.jsx
@@ -1,8 +1,9 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Box, Grid } from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { MAJORS_LIST } from "../utils/constants";
 import Breadcrumb from "./Breadcrumb";
+
 function MajorPage() {
   const params = useParams();
   let data = [];
@@ -29,6 +30,32 @@ function MajorPage() {
       >
         {data.name} (Major)
       </Typography>
+      <Grid container>
+        <Grid item>
+          <Box
+            sx={{
+              backgroundColor: "#F3E8DE",
+              borderRadius: "36px",
+              padding: "2.25rem",
+              margin: {
+                xs: "0.75rem",
+                sm: "1.25rem",
+                md: "1.75rem",
+              },
+              flexBasis: "30%",
+            }}
+          >
+            <Typography variant="h3" sx={{ paddingBottom: "1.5rem" }}>
+              What is{" "}
+              {["a", "e", "i", "o", "u"].includes(data.name[0].toLowerCase())
+                ? "an "
+                : "a "}
+              {data.name} Major?
+            </Typography>
+            <Typography variant="CustomOverline">{data.desc}</Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
