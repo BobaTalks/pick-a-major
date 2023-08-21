1a032851e40ff7ac397d241cffa6f5d8160c6e27
Raymond <raymondsetrau@gmail.com>
2023-08-21 14:59:52 -0700

    change quize button styling

--- tmt-app/src/Components/QuizButtonContainer.jsx
+++ tmt-app/src/Components/QuizButtonContainer.jsx
@@ -1,8 +1,7 @@
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Button, Link } from '@mui/material';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { PrimaryBtn } from '../Styling/CustomStyling.js';
 function QuizButtonContainer({ step }) {
   return (
     <Box
@@ -20,28 +19,9 @@ function QuizButtonContainer({ step }) {
         component={RouterLink}
         to="/quiz/skills"
       >
-        <PrimaryBtn
-          sx={{
-            backgroundColor: 'white',
-            color: 'Matcha.main',
-            border: '3px solid #73956F',
-            padding: {
-              xs: '1rem 1.75rem',
-              md: '1.25rem 4.25rem',
-            },
-            '&:hover': {
-              color: 'white',
-              border: '3px solid #732C2C',
-            },
-          }}
-        >
-          <Typography variant="h4" sx={{ visibility: 'hidden' }}>
-            {step === 'classes' ? 'Next Question' : "I'm Finished"}
-          </Typography>
-          <Typography variant="h4" sx={{ position: 'absolute' }}>
-            Back
-          </Typography>
-        </PrimaryBtn>
+        <Button variant="quizButton" color="secondary">
+          Back
+        </Button>
       </Link>
       <Link
         sx={{ color: 'Boba.main' }}
@@ -50,20 +30,9 @@ function QuizButtonContainer({ step }) {
         component={RouterLink}
         to="/quiz/skills"
       >
-        <PrimaryBtn
-          sx={{
-            backgroundColor: 'Matcha.main',
-            border: '3px solid white',
-            padding: {
-              xs: '1rem 1.75rem',
-              md: '1.25rem 4.25rem',
-            },
-          }}
-        >
-          <Typography variant="h4">
-            {step === 'classes' ? 'Next Question' : "I'm Finished"}
-          </Typography>
-        </PrimaryBtn>
+        <Button variant="quizButton" color="primary">
+          {step === 'classes' ? 'Next Question' : "I'm Finished"}
+        </Button>
       </Link>
     </Box>
   );
